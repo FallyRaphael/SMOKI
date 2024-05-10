ce323c248f26c24372500bf14f0ab5b8db76ccbe
FallyRaphael <raphaelfally@gmail.com>
2024-05-10 12:40:46 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>smoki</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import Pageviewer from './components/Pageviewer.vue';
+
+  export default {
+    components: {
+      Pageviewer
+    },
+    data() {
+      return {
+        activePage: 0,
+        pages: [
+          {
+            id: 1,
+            pageTitle: 'Home',
+            content: 'Welcome to the home page'
+          },
+          {
+            id: 2,
+            pageTitle: 'About',
+            content: 'Learn more about us'
+          },
+          {
+            id: 3,
+            pageTitle: 'Contact',
+            content: 'Get in touch with us'
+          }
+        ],
+        currentPage: {}
+      }
+    },
+    }
+  }
+</script>
