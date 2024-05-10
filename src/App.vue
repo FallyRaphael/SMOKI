aef141e9765d77d7d3f926e17699ffb3164b80a7
FallyRaphael <raphaelfally@gmail.com>
2024-05-10 16:07:49 +0200

    footer, basic navbar, install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,49 @@
 <template>
-  <div>
-    <p>smoki</p>
-  </div>
+    <v-app>
+        <v-main>
+            <navBar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index">
+
+            </navBar>
+            <page-viewer :page="pages[activePage]"></page-viewer>
+        </v-main>
+        <app-footer> </app-footer>
+    </v-app>
 </template>
 
-<style scoped>
-
-</style>
-
 <script>
-import Pageviewer from './components/Pageviewer.vue';
+import PageViewer from './components/PageViewer.vue';
+import NavBar from './components/NavBar.vue';
+import AppFooter from './components/AppFooter.vue';
 
-  export default {
+
+export default {
     components: {
-      Pageviewer
+        PageViewer,
+        NavBar,
+        AppFooter
     },
     data() {
-      return {
-        activePage: 0,
-        pages: [
-          {
-            id: 1,
-            pageTitle: 'Home',
-            content: 'Welcome to the home page'
-          },
-          {
-            id: 2,
-            pageTitle: 'About',
-            content: 'Learn more about us'
-          },
-          {
-            id: 3,
-            pageTitle: 'Contact',
-            content: 'Get in touch with us'
-          }
-        ],
-        currentPage: {}
-      }
+        return {
+            activePage: 0,
+            useDarkNav: false,
+            pages: [
+                {
+                    link: { text: "Home", url: "index.html" },
+                    pageTitle: "Home Page",
+                    content: "This is the home content",
+                },
+                {
+                    link: { text: "About", url: "about.html" },
+                    pageTitle: "About Page",
+                    content: "This is the About content",
+                },
+                {
+                    link: { text: "Contact", url: "contact.html" },
+                    pageTitle: "Contact Page",
+                    content: "This is the Contact content",
+                },
+            ],
+        };
     },
-    }
-  }
+}
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,42 @@
 <template>
-    
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li v-for="(page, index) in pages" class="nav-item" :key="index">
+            <nav-bar-link
+              :page="page"
+              :isActive="activePage == index"
+              @click.prevent="navLinkClick(index)"></nav-bar-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button class="btn btn-primary" @click.prevent="changeTheme()">
+            Toggle nav
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+
 </template>
 
 <script>
-    export default {
-        props: ['page']
+import NavBarLink from './NavBarLink.vue'
+export default {
+  components: { NavBarLink },
+  props: ['pages', 'activePage', 'navLinkClick'],
+  methods: {
+    changeTheme() {
+      this.useDarkNav = !this.useDarkNav;
     }
+  },
+}
+
 </script>
