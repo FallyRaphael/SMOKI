5eccb3e22090d391d3ecaafa1bab774c02ddb710
FallyRaphael <raphaelfally@gmail.com>
2024-05-14 12:44:58 +0200

    reg

--- src/components/EventRegister.vue
+++ src/components/EventRegister.vue
@@ -6,23 +6,24 @@
           <v-card-title>Register an Event</v-card-title>
           <v-card-text>
             <v-form ref="form" v-model="isFormValid">
-              <v-text-field label="Event Name" v-model="eventName" :rules="[rules.required, rules.maxLength(50)]"
-                required></v-text-field>
-              <v-text-field label="Event Date" v-model="eventDate" :rules="[rules.required, rules.dateFormat]"
-                required></v-text-field>
-              <v-text-field label="Event Time" v-model="eventTime" :rules="[rules.required, rules.timeFormat]"
-                required></v-text-field>
-              <v-text-field label="Ticket Price" v-model="ticketPrice" :rules="[rules.required, rules.number]"
-                required></v-text-field>
+              <!-- Existing form fields -->
+              <v-text-field label="Event Name" v-model="eventName" :rules="[rules.required, rules.maxLength(50)]" required></v-text-field>
+              <v-text-field label="Event Date" v-model="eventDate" :rules="[rules.required, rules.dateFormat]" required></v-text-field>
+              <v-text-field label="Event Time" v-model="eventTime" :rules="[rules.required, rules.timeFormat]" required></v-text-field>
+              <v-text-field label="Ticket Price" v-model="ticketPrice" :rules="[rules.required, rules.number]" required></v-text-field>
               <v-text-field label="More Info Link" v-model="moreInfoLink" :rules="[rules.url]"></v-text-field>
+              
+              <!-- File input for image upload -->
+              <v-file-input label="Event Image" v-model="eventImage" :rules="[rules.required]" required></v-file-input>
+              
+              <!-- Map and other fields -->
               <div id="map" style="height: 400px;">
                 <l-map style="height: 100%;" :zoom="zoom" :center="mapCenter" @click="setEventLocation">
                   <l-tile-layer :url="tileLayerUrl"></l-tile-layer>
                   <l-marker :lat-lng="eventLocation"></l-marker>
                 </l-map>
               </div>
-              <v-textarea label="Description" v-model="eventDescription" :rules="[rules.required, rules.maxLength(200)]"
-                required></v-textarea>
+              <v-textarea label="Description" v-model="eventDescription" :rules="[rules.required, rules.maxLength(200)]" required></v-textarea>
             </v-form>
           </v-card-text>
           <v-card-actions>
@@ -55,6 +56,7 @@ export default {
       moreInfoLink: '',
       eventLat: '',
       eventLng: '',
+      eventImage: null, // Add this line
       eventDescription: '',
       isFormValid: false,
       mapCenter: [48.2225, 16.4256], // Default center, can be changed
@@ -74,19 +76,22 @@ export default {
   methods: {
     registerEvent() {
       if (this.$refs.form.validate()) {
-        const geodata = {
-          lat: parseFloat(this.eventLat),
-          lng: parseFloat(this.eventLng)
-        };
-        axios.post('http://localhost:3000/register-event', {
-          eventName: this.eventName,
-          eventDate: this.eventDate,
-          eventTime: this.eventTime,
-          ticketPrice: parseFloat(this.ticketPrice),
-          moreInfoLink: this.moreInfoLink,
-          eventImage: "/viennacruising.jpg", // Handle this appropriately for file upload
-          eventGeodata: geodata,
-          eventDescription: this.eventDescription
+        const formData = new FormData();
+        formData.append('eventName', this.eventName);
+        formData.append('eventDate', this.eventDate);
+        formData.append('eventTime', this.eventTime);
+        formData.append('ticketPrice', this.ticketPrice);
+        formData.append('moreInfoLink', this.moreInfoLink);
+        formData.append('eventGeodata', JSON.stringify({ lat: this.eventLat, lng: this.eventLng }));
+        formData.append('eventDescription', this.eventDescription);
+        if (this.eventImage) {
+          formData.append('eventImage', this.eventImage);
+        }
+
+        axios.post('http://localhost:3000/register-event', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
         })
         .then(response => {
           alert('Event registered successfully!');
@@ -107,6 +112,7 @@ export default {
       this.eventTime = '';
       this.ticketPrice = '';
       this.moreInfoLink = '';
+      this.eventImage = null;
       this.eventLat = '';
       this.eventLng = '';
       this.eventDescription = '';
